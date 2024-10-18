b9657fdc5710a9ced050051d6b169c25b204a7e5
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2024-10-18 13:51:12 +0900

    シーン名を定義

---
+++ Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SceneNames
+{
+    public const string TITLE = "TitleScene";
+    public const string HOME = "HomeScene";
+    public const string GAME = "GameScene";
+}
