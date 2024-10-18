6f256e7e4c56682d01026cadf542c73634335c17
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2024-10-18 15:58:24 +0900

    Home -> _Complete-Game のシーン遷移実装

---
+++ Assets/_Completed-Assets/Scripts/UI/VersusPlayerButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class VersusPlayerButton : MonoBehaviour
+{
+    [SerializeField] private Button button; // インスペクタへの表示をするためSerializeFieldを付加
+
+    void Start()
+    {
+        button.onClick.AddListener(this.onClicked); // ボタンクリック時に実行される関数を指定
+    }
+
+    void onClicked()
+    {
+        SceneManager.LoadScene(SceneNames.GAME); // ゲーム画面への遷移
+    }
+}
