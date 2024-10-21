7fbd20d140af2da9d542c330161afc04fec3636f
MoriguchiHinata <h.moriguchi.23.5.966@4nonome.com>
2024-10-21 23:45:29 +0900

    不要なnamespaceの削除

--- Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
+++ Assets/_Completed-Assets/Scripts/Definitions/SceneNames.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class SceneNames
+﻿public static class SceneNames
 {
     public const string TITLE = "TitleScene";
     public const string HOME = "HomeScene";
