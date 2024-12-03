e143b6a6a8c765d2ecd65e9a686bf73a057f314f
EveningPalace-Sky <6320096@ed.tus.ac.jp>
2024-12-03 16:29:57 +0900

    ワームホールの処理(実機稼働確認済み)

--- Assets/_Completed-Assets/Scripts/Tank/TankController.cs
+++ Assets/_Completed-Assets/Scripts/Tank/TankController.cs
@@ -1,58 +1,27 @@
-using System.Collections;
 using UnityEngine;
 
 public class TankController : MonoBehaviour
 {
-    public bool IsWarping { get; private set; }
-    private Renderer tankRenderer;
+    public bool IsWarping { get; set; } // 現在ワープ中かどうか
+
     private Collider tankCollider;
-    private float originalFireCooldown;
 
     private void Start()
     {
-        tankRenderer = GetComponent<Renderer>();
         tankCollider = GetComponent<Collider>();
     }
 
-    public void StartWarping(float duration)
+    public void DisableCombat()
     {
-        IsWarping = true;
-        StartCoroutine(WarpEffect(duration));
-        DisableCombat();
+        // 無敵状態および攻撃無効
+        tankCollider.enabled = false;
+        // 必要に応じて砲弾や地雷の設置を無効化
     }
 
-    private IEnumerator WarpEffect(float duration)
+    public void EnableCombat()
     {
-        float elapsed = 0f;
-        bool isVisible = true;
-
-        while (elapsed < duration)
-        {
-            isVisible = !isVisible;
-            tankRenderer.enabled = isVisible; // 点滅効果
-            elapsed += 0.2f;
-            yield return new WaitForSeconds(0.2f);
-        }
-
-        tankRenderer.enabled = true; // 点滅終了
-    }
-
-    public void EndWarping()
-    {
-        IsWarping = false;
-        EnableCombat();
-    }
-
-    private void DisableCombat()
-    {
-        // 戦車の攻撃を無効化するロジック
-        tankCollider.enabled = false; // 無敵状態
-        // 砲弾や地雷の設置を無効化 (必要に応じてアタッチするコンポーネントに応じて処理)
-    }
-
-    private void EnableCombat()
-    {
-        tankCollider.enabled = true; // 無敵解除
-        // 攻撃を再度有効化
+        // 無敵解除および攻撃再開
+        tankCollider.enabled = true;
+        // 必要に応じて砲弾や地雷の設置を再有効化
     }
 }
