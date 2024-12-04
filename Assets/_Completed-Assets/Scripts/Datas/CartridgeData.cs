911ad42138030e462c7da250dfde686ca17eeb90
MoriguchiHinata <forest.tp03@gmail.com>
2024-12-04 15:48:08 +0900

    カートリッジを管理するクラスの作成

---
+++ Assets/_Completed-Assets/Scripts/Datas/CartridgeData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable] public class CartridgeData
+{
+    public GameObject m_CartridgePrefab;        // カートリッジのプレハブ
+    public float m_CartridgeSpawnArea = 100f;   // カートリッジの生成範囲（正方形の一辺）
+    public float m_SpawnHeight = 0.5f;          // 生成する高さ
+    public float m_CartridgeSpawnInterval = 5f; // カートリッジの生成間隔
+}
