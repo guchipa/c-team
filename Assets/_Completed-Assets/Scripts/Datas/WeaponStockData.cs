6336c2d51108f736dad3ae55c3a4254921267e68
MoriguchiHinata <forest.tp03@gmail.com>
2024-12-09 22:23:47 +0900

    弾薬管理クラスの追加

---
+++ Assets/_Completed-Assets/Scripts/Datas/WeaponStockData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WeaponStockData
+{
+    [SerializeField] private int initialAmmoCount;
+    [SerializeField] private int maxAmmoCount;
+    [SerializeField] private int addAmmoCount;
+
+    private int currentAmmoCount;
+
+    public int CurrentAmmoCount
+    {
+        get { return currentAmmoCount; }
+    }
+
+    public void Initialize()
+    {
+        currentAmmoCount = initialAmmoCount;
+    }
+
+    public void AddAmmo()
+    {
+        currentAmmoCount += addAmmoCount;
+        if (currentAmmoCount > maxAmmoCount)
+        {
+            currentAmmoCount = maxAmmoCount;
+        }
+    }
+
+    public void UseAmmo()
+    {
+        currentAmmoCount--;
+        if (currentAmmoCount < 0)
+        {
+            currentAmmoCount = 0;
+        }
+    }
+}
